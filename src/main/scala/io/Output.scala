719b53405799e40ffe39b286db3dfc423d7faf88
KasonChan <kasonl.chan@gmail.com>
2015-10-25 13:14:05 -0500

    Add Unit and Output

---
+++ src/main/scala/io/Output.scala
@@ -0,0 +1,10 @@
+package io
+
+/**
+ * Created by kasonchan on 10/25/15.
+ */
+object Output {
+
+  def apply(value: Double, unit: String): Unit = println(value + " " + unit)
+
+}
---
+++ src/main/scala/valueunit/Unit.scala
@@ -0,0 +1,42 @@
+package valueunit
+
+/**
+ * Created by kasonchan on 10/25/15.
+ */
+object Unit {
+
+  val tssRemovalUnits = List("%")
+
+  val bod5RemovalUnits = List("%")
+
+  val qUnits = List("m^3/d")
+
+  val tssOUnits = List("g^3/m^3")
+
+  val tssEUnits = List("g^3/m^3")
+
+  val bod5OUnits = List("g^3/m^3")
+
+  val bod5EUnits = List("g^3/m^3")
+
+  val bod5_cbod5Units = List("g BOD5/g cBOD5")
+
+  val cod_bodUnits = List("g COD/g BOD")
+
+  val cod_vssUnits = List("g COD/g VSS")
+
+  val vss_tssUnits = List("g VSS/g TSS")
+
+  val vsseUnits = List("g/m^3")
+
+  val cBOD5eUnits = List("g/m^3")
+
+  val bCODeUnits = List("g/m^3")
+
+  val bCODpeUnits = List("g/m^3")
+
+  val bCODsUnits = List("g/m^3")
+
+  val pUnits = List("g TSS/d")
+
+}
