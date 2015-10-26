4245f52962e33a926450eff2bfdfefe695dbb708
KasonChan <kasonl.chan@gmail.com>
2015-10-26 16:48:40 -0500

    Fix error prone; Add new ValueUnit

--- src/main/scala/objects/pretreatment/screen/ValueUnits.scala
+++ src/main/scala/objects/pretreatment/screen/ValueUnits.scala
@@ -1,46 +1,58 @@
 package objects.pretreatment.screen
 
-import objects.pretreatment.screen.Unit._
 import valueunit.ValueUnit
 
 /**
  * Created by kasonchan on 10/26/15.
  */
 case class TSSRemoval(value: Double,
-                      unit: String = tssRemovalUnits.head) extends ValueUnit
+                      unit: String = "%") extends ValueUnit
 
 case class BOD5Removal(value: Double,
-                       unit: String = bod5RemovalUnits.head) extends ValueUnit
+                       unit: String = "%") extends ValueUnit
 
 case class Q(value: Double,
-             unit: String = qUnits.head) extends ValueUnit
+             unit: String = "m^3/d") extends ValueUnit
 
 case class TSSo(value: Double,
-                unit: String = tssOUnits.head) extends ValueUnit
+                unit: String = "g^3/m^3") extends ValueUnit
 
 case class TSSe(value: Double,
-                unit: String = tssEUnits.head) extends ValueUnit
+                unit: String = "g^3/m^3") extends ValueUnit
 
 case class BOD5o(value: Double,
-                 unit: String = bod5OUnits.head) extends ValueUnit
+                 unit: String = "g^3/m^3") extends ValueUnit
 
 case class BOD5e(value: Double,
-                 unit: String = bod5EUnits.head) extends ValueUnit
+                 unit: String = "g^3/m^3") extends ValueUnit
+
+case class BOD5CBOD5(value: Double,
+                     unit: String = "g-BOD5/g-cBOD5") extends ValueUnit
+
+case class CODBODRatio(value: Double,
+                       unit: String = "g-COD/g-BOD") extends ValueUnit
+
+case class CODVSSRation(value: Double,
+                        unit: String = "g-COD/g-VSS") extends ValueUnit
+
+
+case class VSSTSSRatio(value: Double,
+                       unit: String = "g-VSS/g-TSS") extends ValueUnit
 
 case class VSSe(value: Double,
-                unit: String = vsseUnits.head) extends ValueUnit
+                unit: String = "g^3/m^3") extends ValueUnit
 
 case class CBOD5e(value: Double,
-                  unit: String = cBOD5eUnits.head) extends ValueUnit
+                  unit: String = "g^3/m^3") extends ValueUnit
 
 case class BCODe(value: Double,
-                 unit: String = bCODeUnits.head) extends ValueUnit
+                 unit: String = "g^3/m^3") extends ValueUnit
 
 case class BCODpe(value: Double,
-                  unit: String = bCODpeUnits.head) extends ValueUnit
+                  unit: String = "g^3/m^3") extends ValueUnit
 
 case class BCODs(value: Double,
-                 unit: String = bCODsUnits.head) extends ValueUnit
+                 unit: String = "g^3/m^3") extends ValueUnit
 
 case class P(value: Double,
-             unit: String = pUnits.head) extends ValueUnit
+             unit: String = "g-TSS/d") extends ValueUnit
