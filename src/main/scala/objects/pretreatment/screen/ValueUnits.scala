6ec2e5ff3991cb04667bdfa101d1b710c3f195e3
KasonChan <kasonl.chan@gmail.com>
2015-10-26 23:26:57 -0500

    Update Unit

--- src/main/scala/objects/pretreatment/screen/ValueUnits.scala
+++ src/main/scala/objects/pretreatment/screen/ValueUnits.scala
@@ -1,58 +1,59 @@
 package objects.pretreatment.screen
 
+import objects.pretreatment.screen.Unit._
 import valueunit.ValueUnit
 
 /**
  * Created by kasonchan on 10/26/15.
  */
 case class TSSRemoval(value: Double,
-                      unit: String = "%") extends ValueUnit
+                      unit: String = tssRemovalUnits.headOption.getOrElse("%")) extends ValueUnit
 
 case class BOD5Removal(value: Double,
-                       unit: String = "%") extends ValueUnit
+                       unit: String = bod5RemovalUnits.headOption.getOrElse("%")) extends ValueUnit
 
 case class Q(value: Double,
-             unit: String = "m^3/d") extends ValueUnit
+             unit: String = qUnits.headOption.getOrElse("m^3/d")) extends ValueUnit
 
 case class TSSo(value: Double,
-                unit: String = "g^3/m^3") extends ValueUnit
+                unit: String = tssOUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class TSSe(value: Double,
-                unit: String = "g^3/m^3") extends ValueUnit
+                unit: String = tssEUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class BOD5o(value: Double,
-                 unit: String = "g^3/m^3") extends ValueUnit
+                 unit: String = bod5OUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class BOD5e(value: Double,
-                 unit: String = "g^3/m^3") extends ValueUnit
+                 unit: String = bod5EUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class BOD5CBOD5(value: Double,
-                     unit: String = "g-BOD5/g-cBOD5") extends ValueUnit
+                     unit: String = bod5cBOD5Units.headOption.getOrElse("g-BOD5/g-cBOD5")) extends ValueUnit
 
 case class CODBODRatio(value: Double,
-                       unit: String = "g-COD/g-BOD") extends ValueUnit
+                       unit: String = codBODUnits.headOption.getOrElse("g-COD/g-BOD")) extends ValueUnit
 
 case class CODVSSRation(value: Double,
-                        unit: String = "g-COD/g-VSS") extends ValueUnit
+                        unit: String = codVSSUnits.headOption.getOrElse("g-COD/g-VSS")) extends ValueUnit
 
 
 case class VSSTSSRatio(value: Double,
-                       unit: String = "g-VSS/g-TSS") extends ValueUnit
+                       unit: String = vssTSSUnits.headOption.getOrElse("g-VSS/g-TSS")) extends ValueUnit
 
 case class VSSe(value: Double,
-                unit: String = "g^3/m^3") extends ValueUnit
+                unit: String = vsseUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class CBOD5e(value: Double,
-                  unit: String = "g^3/m^3") extends ValueUnit
+                  unit: String = cBOD5eUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class BCODe(value: Double,
-                 unit: String = "g^3/m^3") extends ValueUnit
+                 unit: String = bCODeUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class BCODpe(value: Double,
-                  unit: String = "g^3/m^3") extends ValueUnit
+                  unit: String = bCODpeUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class BCODs(value: Double,
-                 unit: String = "g^3/m^3") extends ValueUnit
+                 unit: String = bCODsUnits.headOption.getOrElse("g^3/m^3")) extends ValueUnit
 
 case class P(value: Double,
-             unit: String = "g-TSS/d") extends ValueUnit
+             unit: String = pUnits.headOption.getOrElse("g-TSS/d")) extends ValueUnit
