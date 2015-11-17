16dd13cfe65e42dc319023e78d02b56e01f1edb0
KasonChan <kasonl.chan@gmail.com>
2015-11-17 12:32:02 -0600

    Add train 0 and 1 to secondaryclarifiers

--- objects/src/test/scala/wastewatertreatment/objects/secondarytreatment/secondaryclarifiers/SecondaryClarifiersSuite.scala
+++ objects/src/test/scala/wastewatertreatment/objects/secondarytreatment/secondaryclarifiers/SecondaryClarifiersSuite.scala
@@ -1,6 +1,7 @@
 package wastewatertreatment.objects.secondarytreatment.secondaryclarifiers
 
 import org.scalatest.{FlatSpec, Matchers}
+import wastewatertreatment.math.Math._
 import wastewatertreatment.objects.secondarytreatment.secondaryclarifiers.SecondaryClarifiers._
 
 /**
@@ -22,6 +23,118 @@ class SecondaryClarifiersSuite extends FlatSpec with Matchers {
 
   "pVSSRatio" should "= 0.02" in {
     pVSSRatio shouldBe 0.02
+  }
+
+  "Train 0" should "pass" in {
+    val q = 481131.12
+    val tss = 0.41000000000000003
+    val vss = 0.32800000000000007
+    val bod5 = 0.7499999999999999
+    val cBOD5 = 0.6818181818181817
+    val bCOD = 1.2
+    val bCODs = 0.8273919999999999
+    val bCODp = 0.37260800000000005
+    val nh3n = 2.97
+    val tp = 0.64
+    val p = 197263.7592
+
+    val qe = q
+    qe shouldBe q
+
+    val tsse = solve(List(MX(Some(q), Some(tss), None)),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(tsse) shouldBe 0.41
+    tsse shouldBe tss
+
+    val vsse = calVSS(tsse)
+    toXDecimals(vsse) shouldBe 0.33
+    vsse shouldBe vss
+
+    val bod5e = solve(List(MX(Some(q), Some(bod5), None)),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(bod5e) shouldBe 0.75
+    bod5e shouldBe bod5
+
+    val cBOD5e = calcBOD5(bod5e)
+    toXDecimals(cBOD5e) shouldBe 0.68
+    cBOD5e shouldBe cBOD5
+
+    val bCODe = calbCOD(bod5e)
+    toXDecimals(bCODe) shouldBe 1.2
+    bCODe shouldBe bCOD
+
+    val bCODpe = calbCODp(vsse)
+    toXDecimals(bCODpe) shouldBe 0.37
+    bCODpe shouldBe bCODp
+
+    val bCODse = calbCODs(bCODe, bCODpe)
+    toXDecimals(bCODse) shouldBe 0.83
+    bCODse shouldBe bCODs
+
+    val nh3ne = solve(List(MX(Some(q), Some(nh3n))),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(nh3ne) shouldBe 2.97
+    nh3ne shouldBe nh3n
+
+    val tpe = solve(List(MX(Some(q), Some(tp))),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(tpe) shouldBe 0.64
+    tpe shouldBe tp
+
+    val pe = calP(qe, tsse)
+    toXDecimals(pe) shouldBe 197263.76
+    pe shouldBe p
+  }
+
+  "Train 1" should "pass" in {
+    val q = 481131.12
+    val tss = 0.41000000000000003
+    val vss = 0.32800000000000007
+    val bod5 = 0.7499999999999999
+    val cBOD5 = 0.6818181818181817
+    val bCOD = 1.2
+    val bCODs = 0.8273919999999999
+    val bCODp = 0.37260800000000005
+    val nh3n = 2.97
+    val tp = 0.64
+    val p = 197263.7592
+
+    val qe = q
+    qe shouldBe q
+
+    val tsse = solve(List(MX(Some(q), Some(tss), Some(tssRemoval))),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(tsse) shouldBe 0.23
+
+    val vsse = calVSS(tsse)
+    toXDecimals(vsse) shouldBe 0.18
+
+    val bod5e = solve(List(MX(Some(q), Some(bod5), Some(bodRemoval))),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(bod5e) shouldBe 0.56
+
+    val cBOD5e = calcBOD5(bod5e)
+    toXDecimals(cBOD5e) shouldBe 0.51
+
+    val bCODe = calbCOD(bod5e)
+    toXDecimals(bCODe) shouldBe 0.9
+
+    val bCODpe = calbCODp(vsse)
+    toXDecimals(bCODpe) shouldBe 0.20
+
+    val bCODse = calbCODs(bCODe, bCODpe)
+    toXDecimals(bCODse) shouldBe 0.70
+
+    val nh3ne = solve(List(MX(Some(q), Some(nh3n))),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(nh3ne) shouldBe 2.97
+
+    val tpe = solve(List(MX(Some(q), Some(tp))),
+      List(MX(Some(qe), None))).getOrElse(0.00)
+    toXDecimals(tpe) shouldBe 0.64
+
+    val pe = calP(qe, tsse)
+    toXDecimals(pe) shouldBe 108495.07
   }
 
 }
