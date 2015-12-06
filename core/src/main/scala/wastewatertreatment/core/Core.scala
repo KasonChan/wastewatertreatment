37b2297d7137f439943d83fd2709ebe9d516b0b8
KasonChan <kasonl.chan@gmail.com>
2015-12-05 22:26:16 -0600

    Add generalized functions to core

--- core/src/main/scala/wastewatertreatment/core/Core.scala
+++ core/src/main/scala/wastewatertreatment/core/Core.scala
@@ -41,6 +41,25 @@ trait Core extends Ratios {
   }
 
   /**
+   * Returns VSS or TSS.
+   * {{{
+   *   VSS = TSS * vssTSSRatio
+   * }}}
+   * @param VSS the initial value of VSS. Default value is 0.00.
+   * @param TSS the initial value of TSS. Default value is 0.00.
+   * @param vssTSS VSS/TSS. Default value and unit are 0.80.
+   */
+  def calVSSTSS(VSS: Option[Double] = None,
+                TSS: Option[Double] = None,
+                vssTSS: Option[Double] = Some(vssTSSRatio)): Double = {
+    require(VSS.getOrElse(0.0) >= 0 &&
+      TSS.getOrElse(0.0) >= 0 &&
+      vssTSS.getOrElse(0.0) >= 0)
+    val r = solveM(List(VSS), List(TSS, vssTSS), 'multiple).getOrElse(0.0)
+    r
+  }
+
+  /**
    * Returns cBOD,,5,,.
    * {{{
    *   cBOD5 = BOD / bod5cBOD5Ratio
@@ -56,6 +75,25 @@ trait Core extends Ratios {
   }
 
   /**
+   * Returns cBOD,,5,, or BOD,,5,,.
+   * {{{
+   *   cBOD5 * bod5cBOD5Ratio = BOD5
+   * }}}
+   * @param cBOD5 the initial value of cBOD,,5,,. Default value is 0.00.
+   * @param bod5cBOD5 BOD,,5,,/cBOD,,5,,. Default value and unit are 1.10.
+   * @param BOD5 the initial value of BOD,,5,,. Default value is 0.00.
+   */
+  def calcBOD5BOD5(cBOD5: Option[Double] = None,
+                   bod5cBOD5: Option[Double] = Some(bod5cBOD5Ratio),
+                   BOD5: Option[Double] = None): Double = {
+    require(cBOD5.getOrElse(0.0) >= 0 &&
+      bod5cBOD5.getOrElse(0.0) > 0 &&
+      BOD5.getOrElse(0.0) >= 0)
+    val r = solveM(List(cBOD5, bod5cBOD5), List(BOD5), 'multiple).getOrElse(0.0)
+    r
+  }
+
+  /**
    * Returns bCOD.
    * {{{
    *   bCOD = BOD5 * codBODRatio
@@ -67,6 +105,25 @@ trait Core extends Ratios {
               codBODRatio: Double = codBODRatio): Double = {
     require(BOD5 >= 0 && codBODRatio >= 0)
     val r = solveM(List(None), List(Some(BOD5), Some(codBODRatio)), 'multiple).getOrElse(0.0)
+    r
+  }
+
+  /**
+   * Returns bCOD or BOD,,5,,.
+   * {{{
+   *   bCOD = BOD5 * codBODRatio
+   * }}}
+   * @param bCOD the initial value of bCOD. Default value is 0.00.
+   * @param BOD5 the initial value of BOD,,5,,. Default value is 0.00
+   * @param codBOD COD/BOD. Default value and unit are 1.60.
+   */
+  def calbCODBOD5(bCOD: Option[Double] = None,
+                  BOD5: Option[Double] = None,
+                  codBOD: Option[Double] = Some(codBODRatio)): Double = {
+    require(bCOD.getOrElse(0.0) >= 0 &&
+      BOD5.getOrElse(0.0) >= 0 &&
+      codBOD.getOrElse(0.0) >= 0)
+    val r = solveM(List(bCOD), List(BOD5, codBOD), 'multiple).getOrElse(0.0)
     r
   }
 
@@ -88,6 +145,28 @@ trait Core extends Ratios {
   }
 
   /**
+   * Returns bCODp or VSS.
+   * {{{
+   *   bCODp = VSS * codVSSRatio * bvssVSSRatio
+   * }}}
+   * @param bCODp the initial value of cBODp. Default value is 0.00.
+   * @param VSS the initial value of VSS. Default value is 0.00
+   * @param codVSS COD/VSS. Default value and unit are 1.42.
+   * @param bvssVSS bVSS/VSS. Default value and unit are 0.80.
+   */
+  def calbCODpVSS(bCODp: Option[Double] = None,
+                  VSS: Option[Double] = None,
+                  codVSS: Option[Double] = Some(codVSSRatio),
+                  bvssVSS: Option[Double] = Some(bvssVSSRatio)): Double = {
+    require(bCODp.getOrElse(0.0) >= 0 &&
+      VSS.getOrElse(0.0) >= 0 &&
+      codVSS.getOrElse(0.0) >= 0 &&
+      bvssVSS.getOrElse(0.0) >= 0)
+    val r = solveM(List(bCODp), List(VSS, codVSS, bvssVSS), 'multiple).getOrElse(0.0)
+    r
+  }
+
+  /**
    * Returns bCODs.
    * {{{
    *   bCODs = bCOD - bCODp
@@ -103,6 +182,25 @@ trait Core extends Ratios {
   }
 
   /**
+   * Returns bCODs or bCOD or bCODp.
+   * {{{
+   *   bCODs = bCOD - bCODp
+   * }}}
+   * @param bCODs the initial value of bCOD. Default value is 0.00.
+   * @param bCOD the initial value of bCOD. Default value is 0.00.
+   * @param bCODp the initial value of bCODp. Default value is 0.00.
+   */
+  def calbCODsbCODpbCOD(bCODs: Option[Double] = None,
+                        bCODp: Option[Double] = None,
+                        bCOD: Option[Double] = None): Double = {
+    require(bCODs.getOrElse(0.0) >= 0 &&
+      bCODp.getOrElse(0.0) >= 0 &&
+      bCOD.getOrElse(0.0) >= 0)
+    val r = solveM(List(bCODs, bCODp), List(bCOD), 'add).getOrElse(0.0)
+    r
+  }
+
+  /**
    * Returns P.
    * {{{
    *   P = Q * TSS
@@ -114,6 +212,25 @@ trait Core extends Ratios {
            TSS: Double): Double = {
     require(Q >= 0 && TSS >= 0)
     val r = solveM(List(None), List(Some(Q), Some(TSS)), 'multiple).getOrElse(0.0)
+    r
+  }
+
+  /**
+   * Returns P, Q or TSS.
+   * {{{
+   *   P = Q * TSS
+   * }}}
+   * @param P the initial value of Q. Default value is 0.00.
+   * @param Q the initial value of Q. Default value is 0.00.
+   * @param TSS the initial value of TSS. Default value is 0.00.
+   */
+  def calPQTSS(P: Option[Double] = None,
+               Q: Option[Double] = None,
+               TSS: Option[Double] = None): Double = {
+    require(P.getOrElse(0.0) >= 0 &&
+      Q.getOrElse(0.0) >= 0 &&
+      TSS.getOrElse(0.0) >= 0)
+    val r = solveM(List(P), List(Q, TSS), 'multiple).getOrElse(0.0)
     r
   }
 
@@ -174,6 +291,25 @@ trait Core extends Ratios {
              ntuTSSRatio: Double = ntuTSSRatio): Double = {
     require(TSS >= 0 && ntuTSSRatio >= 0)
     val r = solveM(List(None), List(Some(TSS), Some(ntuTSSRatio)), 'multiple).getOrElse(0.0)
+    r
+  }
+
+  /**
+   * Returns NTU or TSS.
+   * {{{
+   *   NTU = TSS * NTU/TSS
+   * }}}
+   * @param NTU the effluent value of NTU. Default value is 0.00.
+   * @param TSS the effluent value of TSS. Default value is 0.00.
+   * @param ntuTSS NTU/TSS. Default value and unit are 0.50.
+   */
+  def calNTUTSS(NTU: Option[Double] = None,
+                TSS: Option[Double] = None,
+                ntuTSS: Option[Double] = Some(ntuTSSRatio)): Double = {
+    require(NTU.getOrElse(0.0) >= 0 &&
+      TSS.getOrElse(0.0) >= 0 &&
+      ntuTSS.getOrElse(0.0) >= 0)
+    val r = solveM(List(NTU), List(TSS, ntuTSS), 'multiple).getOrElse(0.0)
     r
   }
 
--- core/src/test/scala/wastewatertreatment/core/CoreSuite.scala
+++ core/src/test/scala/wastewatertreatment/core/CoreSuite.scala
@@ -48,6 +48,24 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     calVSS(147.13) shouldBe 117.70400000000001
   }
 
+  "calVSSTSS(TSS = Some(-1), vssTSS = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calVSSTSS(TSS = Some(-1), vssTSS = Some(-1))
+    }
+  }
+
+  "calVSSTSS(TSS = Some(0.0), vssTSS = Some(0.0)" should "= 0.0" in {
+    calVSSTSS(TSS = Some(0.0), vssTSS = Some(0.0)) shouldBe 0
+  }
+
+  "calVSSTSS(TSS = Some(147.13), vssTSS = Some(0.8)" should "= 117.70400000000001" in {
+    calVSSTSS(TSS = Some(147.13), vssTSS = Some(0.8)) shouldBe 117.70400000000001
+  }
+
+  "calVSSTSS(TSS = Some(147.13))" should "= 117.70400000000001" in {
+    calVSSTSS(TSS = Some(147.13)) shouldBe 117.70400000000001
+  }
+
   "calcBOD5(-1, -1)" should "fail" in {
     intercept[java.lang.IllegalArgumentException] {
       calcBOD5(-1, -1)
@@ -71,6 +89,29 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     calcBOD5(cBOD5) shouldBe 154.23636363636362
   }
 
+  "calcBOD5BOD5(BOD5 = Some(-1), bod5cBOD5 = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calcBOD5BOD5(BOD5 = Some(-1), bod5cBOD5 = Some(-1))
+    }
+  }
+
+  "calcBOD5BOD5(BOD5 = Some(0.0), bod5cBOD5 = Some(0.0))" should "= 0.0" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calcBOD5BOD5(BOD5 = Some(0.0), bod5cBOD5 = Some(0.0))
+    }
+  }
+
+  "calcBOD5BOD5(cBOD5, 1.1)" should "= 154.23636363636362" in {
+    val BOD5 = Some(169.66)
+    val bod5cBOD5Ratio = Some(1.1)
+    calcBOD5BOD5(BOD5 = BOD5, bod5cBOD5 = bod5cBOD5Ratio) shouldBe 154.23636363636362
+  }
+
+  "calcBOD5BOD5(cBOD5)" should "= 154.23636363636362" in {
+    val BOD5 = Some(169.66)
+    calcBOD5BOD5(BOD5 = BOD5) shouldBe 154.23636363636362
+  }
+
   "calbCOD(-1, -1)" should "fail" in {
     intercept[java.lang.IllegalArgumentException] {
       calbCOD(-1, -1)
@@ -90,6 +131,27 @@ class CoreSuite extends FlatSpec with Matchers with Core {
   "calbCOD(bod5)" should "= 271.456" in {
     val bod5 = 169.66
     calbCOD(bod5) shouldBe 271.456
+  }
+
+  "calbCODBOD5(BOD5 = Some(-1), codBOD = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calbCODBOD5(BOD5 = Some(-1), codBOD = Some(-1))
+    }
+  }
+
+  "calbCODBOD5(BOD5 = Some(0), codBOD = Some(0))" should "= 0.0" in {
+    calbCODBOD5(BOD5 = Some(0), codBOD = Some(0)) shouldBe 0
+  }
+
+  "calbCODBOD5(bod5, 1.6)" should "= 271.456" in {
+    val bod5 = Some(169.66)
+    val codBODRatio = Some(1.6)
+    calbCODBOD5(BOD5 = bod5, codBOD = codBODRatio) shouldBe 271.456
+  }
+
+  "calbCODBOD5(bod5)" should "= 271.456" in {
+    val bod5 = Some(169.66)
+    calbCODBOD5(BOD5 = bod5) shouldBe 271.456
   }
 
   "calbCODp(-1, -1, -1)" should "fail" in {
@@ -114,6 +176,28 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     calbCODp(vss) shouldBe 133.71856
   }
 
+  "calbCODpVSS(VSS = Some(-1), codVSS = Some(-1), bvssVSS = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calbCODpVSS(VSS = Some(-1), codVSS = Some(-1), bvssVSS = Some(-1))
+    }
+  }
+
+  "calbCODpVSS(VSS = Some(0), codVSS = Some(0), bvssVSS = Some(0))" should "= 0.0" in {
+    calbCODpVSS(VSS = Some(0), codVSS = Some(0), bvssVSS = Some(0)) shouldBe 0
+  }
+
+  "calbCODpVSS(vss, codVSSRatio, bVSS)" should "= 133.71856" in {
+    val vss = Some(117.71)
+    val codVSSRatio = Some(1.42)
+    val bVSS = Some(0.8)
+    calbCODpVSS(VSS = vss, codVSS = codVSSRatio, bvssVSS = bVSS) shouldBe 133.71856
+  }
+
+  "calbCODpVSS(vss)" should "= 133.71856" in {
+    val vss = Some(117.71)
+    calbCODpVSS(VSS = vss) shouldBe 133.71856
+  }
+
   "calbCODs(-1, -1)" should "fail" in {
     intercept[java.lang.IllegalArgumentException] {
       calbCODs(-1, -1)
@@ -130,6 +214,22 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     calbCODs(bCOD, bCODp) shouldBe 137.74999999999997
   }
 
+  "calbCODsbCODpbCOD(bCOD = Some(-1), bCODp = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calbCODsbCODpbCOD(bCOD = Some(-1), bCODp = Some(-1))
+    }
+  }
+
+  "calbCODsbCODpbCOD(bCOD = Some(0), bCODp = Some(0))" should "= 0.0" in {
+    calbCODsbCODpbCOD(bCOD = Some(0), bCODp = Some(0)) shouldBe 0
+  }
+
+  "calbCODsbCODpbCOD(bCOD = Some(bCOD), bCODp = bCODp)" should "= 137.74999999999997" in {
+    val bCOD = Some(271.46)
+    val bCODp = Some(133.71)
+    calbCODsbCODpbCOD(bCOD = bCOD, bCODp = bCODp) shouldBe 137.74999999999997
+  }
+
   "calP(-1, -1)" should "fail" in {
     intercept[java.lang.IllegalArgumentException] {
       calP(-1, -1)
@@ -144,6 +244,22 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     val q = 1240973.21
     val tss = 147.13
     calP(q, tss) shouldBe 1.8258438838729998E8
+  }
+
+  "calPQTSS(Q = Some(-1), TSS = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calPQTSS(Q = Some(-1), TSS = Some(-1))
+    }
+  }
+
+  "calPQTSS(Q = Some(0), TSS = Some(0))" should "= 0.0" in {
+    calPQTSS(Q = Some(0), TSS = Some(0)) shouldBe 0
+  }
+
+  "calPQTSS(Q = q, tss)" should "= 1.8258438838729998E8" in {
+    val q = Some(1240973.21)
+    val tss = Some(147.13)
+    calPQTSS(Q = q, TSS = tss) shouldBe 1.8258438838729998E8
   }
 
   "calXPercentage(-1, -1)" should "fail" in {
@@ -180,6 +296,24 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     calQ(p, tss) shouldBe 1996.8231050000002
   }
 
+  "calPQTSS(P = Some(-1), TSS = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calPQTSS(P = Some(-1), TSS = Some(-1))
+    }
+  }
+
+  "calPQTSS(P = Some(0), TSS = Some(0))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calPQTSS(P = Some(0), TSS = Some(0))
+    }
+  }
+
+  "calPQTSS(p, tss)" should "= 1996.8231050000002" in {
+    val p = Some(79872924.20)
+    val tss = Some(40000.0)
+    calPQTSS(P = p, TSS = tss) shouldBe 1996.8231050000002
+  }
+
   "calTSS(-1, -1)" should "fail" in {
     intercept[java.lang.IllegalArgumentException] {
       calTSS(-1, -1)
@@ -198,6 +332,24 @@ class CoreSuite extends FlatSpec with Matchers with Core {
     calTSS(p, q) shouldBe 222.99999999999997
   }
 
+  "calPQTSS(P = Some(-1), Q = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calPQTSS(P = Some(-1), Q = Some(-1))
+    }
+  }
+
+  "calPQTSS(P = Some(0), Q= Some(0))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calPQTSS(P = Some(0), Q= Some(0))
+    }
+  }
+
+  "calPQTSS(P = p, Q = q)" should "= 222.99999999999997" in {
+    val p = Some(140127937.20)
+    val q = Some(628376.40)
+    calPQTSS(P = p, Q = q) shouldBe 222.99999999999997
+  }
+
   "calNTU(-1, -1)" should "fail" in {
     intercept[java.lang.IllegalArgumentException] {
       calNTU(-1, -1)
@@ -211,6 +363,21 @@ class CoreSuite extends FlatSpec with Matchers with Core {
   "calNTU(tss)" should "= " in {
     val tss = 0.94
     calNTU(tss) shouldBe 0.47
+  }
+
+  "calNTUTSS(TSS = Some(-1), ntuTSS = Some(-1))" should "fail" in {
+    intercept[java.lang.IllegalArgumentException] {
+      calNTUTSS(TSS = Some(-1), ntuTSS = Some(-1))
+    }
+  }
+
+  "calNTUTSS(TSS = Some(0), ntuTSS = Some(0))" should "fail" in {
+    calNTUTSS(TSS = Some(0), ntuTSS = Some(0)) shouldBe 0
+  }
+
+  "calNTUTSS(TSS = tss)" should "= " in {
+    val tss = Some(0.94)
+    calNTUTSS(TSS = tss) shouldBe 0.47
   }
 
 }
